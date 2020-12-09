de3cd16f68d6e4aafb9f412d8655cebb35dd8eec
K. Dushyant Reddy <55031190+Dushyant029@users.noreply.github.com>
2020-12-09 16:40:53 +0530

    pointers

--- pointersInC/PointToArray.c
+++ pointersInC/PointToArray.c
@@ -4,13 +4,13 @@ int main() {
     int *myGrades = gradeList;
     printf("1st element of gradelist using array index: %d\n",gradeList[0]);
     printf("1st element of gradelist using pointer: %d\n",*myGrades);
-    printf("2nd element of gradelist using array index: %d\n", *(myGrades + 1));
+    printf("2nd element of gradelist using array index: %d\n", *(myGrades+1));
     printf("2nd element of gradelist using pointer: %d\n", myGrades[1]);
-    printf("3rd element of gradelist using array index: %d\n", *(myGrades + 2));
+    printf("3rd element of gradelist using array index: %d\n", *(myGrades+2));
     printf("3rd element of gradelist using pointer: %d\n", myGrades[2]);
-    printf("4th element of gradelist using array index: %d\n", *(myGrades + 3));
+    printf("4th element of gradelist using array index: %d\n", *(myGrades+3));
     printf("4th element of gradelist using pointer %d\n", myGrades[3]);
-    printf("5th element of gradelist using array index: %d\n", *(myGrades + 4));
+    printf("5th element of gradelist using array index: %d\n", *(myGrades+4));
     printf("5th element of gradelist using pointer: %d\n", myGrades[4]);
     return 0;
 }
